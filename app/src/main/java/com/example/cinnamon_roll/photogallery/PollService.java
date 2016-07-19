32f93809498edaef0d7f6f1503e53be8537a16b0
Cat Tran <trandinhcat@gmail.com>
2016-07-19 13:46:05 -0500

    Implemented notification for new picture

--- app/src/main/java/com/example/cinnamon_roll/photogallery/PollService.java
+++ app/src/main/java/com/example/cinnamon_roll/photogallery/PollService.java
@@ -2,11 +2,16 @@ package com.example.cinnamon_roll.photogallery;
 
 import android.app.AlarmManager;
 import android.app.IntentService;
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Resources;
 import android.net.ConnectivityManager;
 import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 
 import java.util.List;
@@ -16,7 +21,7 @@ import java.util.List;
  */
 public class PollService extends IntentService{
     private static final String TAG = "PollService";
-    private static final int POLL_INTERNAL = 1000*60;//60 seconds
+    private static final long POLL_INTERNAL = AlarmManager.INTERVAL_FIFTEEN_MINUTES;
     //--------------------newIntent()-----------------------//
     public static Intent newIntent(Context context){
         return new Intent(context, PollService.class);
@@ -46,6 +51,7 @@ public class PollService extends IntentService{
         super(TAG);
     }
     //---------------------onHandleIntent()----------------//
+    @Override
     protected void onHandleIntent(Intent intent){
         if(!isNetworkAvailableAndConnected())//it is good practice to check for network connection before downloading data in background
         {
@@ -70,6 +76,20 @@ public class PollService extends IntentService{
         }
         else{
             Log.i(TAG, "Got a new result: "+ resultId);
+            //Using notification to notify user
+            Resources resources = getResources();
+            Intent i = PhotoGalleryActivity.newIntent(this);
+            PendingIntent pi = PendingIntent.getActivity(this, 0, i, 0);
+            Notification notification = new NotificationCompat.Builder(this)
+                    .setTicker(resources.getString(R.string.new_pictures_title))
+                    .setSmallIcon(android.R.drawable.ic_menu_report_image)
+                    .setContentTitle(resources.getString(R.string.new_pictures_title))
+                    .setContentText(resources.getString(R.string.new_pictures_text))
+                    .setContentIntent(pi)
+                    .setAutoCancel(true)
+                    .build();
+            NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+            notificationManagerCompat.notify(0, notification);
         }
         QueryPreferences.setLastResultId(this, resultId);
     }
