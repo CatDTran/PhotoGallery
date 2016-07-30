0c216340e54f6482be48e480cc9c6c4a32b71787
Cat Tran <trandinhcat@gmail.com>
2016-07-30 14:52:12 -0500

    New Class: StartupReceiver. Updated manifest file

---
+++ app/src/main/java/com/example/cinnamon_roll/photogallery/StartupReceiver.java
@@ -0,0 +1,18 @@
+package com.example.cinnamon_roll.photogallery;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+/**
+ * Created by trand_000 on 7/30/2016.
+ */
+public class StartupReceiver extends BroadcastReceiver{
+    private static final String TAG = "StartupReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent){
+        Log.i(TAG, "Received broadcast intent: "+ intent.getAction());
+    }
+}
